ff9cc9836c2ee2b63bfee9d49521daebcf0bf745
Jock Dalby <jockdalby86@gmail.com>
2017-04-27 14:32:31 +1200

    setup basic component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UserSelectionComponent } from './user-selection/user-selection.component';
+import { ThreadSelectionComponent } from './thread-section/thread-selection.component';
+import { MessageSectionComponent } from './message-section/message-section.component';
+import { ThreadListComponent } from './thread-section/thread-list/thread-list.component';
+import { MessageListComponent } from './message-section/message-list/message-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserSelectionComponent,
+    ThreadSelectionComponent,
+    MessageSectionComponent,
+    ThreadListComponent,
+    MessageListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/thread-section/thread-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'thread-selection',
+  templateUrl: './thread-selection.component.html',
+  styleUrls: ['./thread-selection.component.css']
+})
+export class ThreadSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
