33f8e421ca7f1199eab7c2a38ab085a28d21054d
Jock Dalby <jockdalby86@gmail.com>
2017-04-29 13:39:40 +1200

    implement auto scrolldown on newMessage

--- src/app/message-section/message-list/message-list.component.ts
+++ src/app/message-section/message-list/message-list.component.ts
@@ -1,18 +1,44 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, Input, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
 import {MessageVM} from '../message.vm';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnChanges {
 
   @Input() messages: MessageVM[];
 
+  @ViewChild('list') list: ElementRef;
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+
+    // Detect if there were changes in the 'messages' input property
+    if (changes['messages']) {
+
+      const previousMessages = changes['messages'].previousValue;
+
+      const newMessages = changes['messages'].currentValue;
+
+      if ((newMessages && previousMessages) && newMessages.length > previousMessages.length) {
+        setTimeout(() => {
+          this.scrollLastMessageIntoView();
+        });
+      }
+    }
+  }
+
+  scrollLastMessageIntoView() {
+    const items = this.list.nativeElement.querySelectorAll('li');
+    const lastItem: any = _.last(items);
+    if (lastItem) {
+      lastItem.scrollIntoView();
+    }
   }
 
 }
